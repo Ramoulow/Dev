84dc83dc4804f41247f7616be3344418491d794e
Coudray Rémi <remi.coudray@outlook.fr>
2022-05-31 13:42:28 +0200

    Modifications relatives à l'exercice web responsive

---
+++ exercices/javascript/js/javascript.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JAVASCRIPT</title>
+
+</head>
+<body>
+
+    <h1>Ma page HTML qui contient un fichier .js</h1><br>
+
+    <h1>qui se trouve dans une balise script en fin de page, avant la femeture de la balise body</h1>
+
+
+    <script src="js/javascript.js"></script>
+
+</body>
+</html>
